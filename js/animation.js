8dbfbfa0abca7ad2b7baebcfed7dd85ce2234fc7
Marc-Antoine Déziel <dezielmarcantoine99@gmail.com>
2019-02-25 12:21:36 -0500

    implementation de la deuxieme animation

--- js/animation.js
+++ js/animation.js
@@ -1,5 +1,7 @@
 window.addEventListener('load', function(){
     /*console.log('fichier animation');*/
     let anim1 = document.querySelectorAll('.site-title');
+    let anim2 = document.querySelectorAll('.site-description');
     anim1[0].classList.add('classeAnim1');
+    anim2[0].classList.add('classeAnim2');
 })
