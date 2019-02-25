4aa68b3e8f13cdbdd4557d3e132ca1c3b332523b
Marc-Antoine Déziel <dezielmarcantoine99@gmail.com>
2019-02-25 12:17:38 -0500

    implementation de la premiere animation

--- js/animation.js
+++ js/animation.js
@@ -1,3 +1,5 @@
 window.addEventListener('load', function(){
-    console.log('fichier animation');
+    /*console.log('fichier animation');*/
+    let anim1 = document.querySelectorAll('.site-title');
+    anim1[0].classList.add('classeAnim1');
 })
